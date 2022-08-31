6b009515b34ccc2e29f55e97ed4c480945992f04
kianamcc <42532725+kianamcc@users.noreply.github.com>
2022-08-31 16:44:35 -0700

    styling of start page, new Quiz component

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Quizz.css";
+
+const Quiz = () => {
+  <div className="quiz">Quiz</div>;
+};
+
+export default Quiz;
--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import "./StartScreen";
+import "./StartScreen.css";
 
 const StartScreen = () => {
-  return <h1>Quizzical</h1>;
+  return (
+    <div className="start-screen">
+      <h1 className="start-title">Quizzical</h1>
+      <p className="start-description">Some description if needed</p>
+      <button className="start-btn">Start Quiz</button>
+    </div>
+  );
 };
 
 export default StartScreen;
